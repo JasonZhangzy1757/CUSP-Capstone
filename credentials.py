beb4c06ef83d71a7566503b7f1b70bf16af3cc20
jasonzhangzy1920@gmail.com <jasonzhangzy1920@gmail.com>
2020-06-18 16:07:34 -0400

    CityIQ Portland Vehicle & Pedestrian Count Aggregation Notebooks Updated

--- credentials.py
+++ credentials.py
@@ -1,17 +1,34 @@
 # Please populate this file with your credentials provided by your city.  
+#credentials = {
+#    "City": { # city key is for clients with multiple tenant. it is recommended to leave the structure as is.
+#        "name" :"San Diego",
+#        "uaa": "https://auth.aa.cityiq.io",
+#        "metadata":"https://sandiego.cityiq.io/api/v2/metadata/",
+#        "event":"https://sandiego.cityiq.io/api/v2/event/",
+#        "websocket": "wss://https://sandiego.cityiq.io/api/v2/websocket", 
+#        "developer": "PublicAccess:qPKIadEsoHjyh226Snz7",
+#        "parking": "SD-IE-PARKING",
+#        "environment": "SD-IE-ENVIRONMENTAL",
+#        "pedestrian": "SD-IE-PEDESTRIAN",
+#        "traffic": "SD-IE-TRAFFIC",
+#        "bicycle": "SD-IE-BIKE", 
+#        "bbox": "33.396515:-117.778637,32.467246:-115.638979"
+#    }
+#}
+
 credentials = {
     "City": { # city key is for clients with multiple tenant. it is recommended to leave the structure as is.
-        "name" :"San Diego",
+        "name" :"Portland",
         "uaa": "https://auth.aa.cityiq.io",
-        "metadata":"https://sandiego.cityiq.io/api/v2/metadata/",
-        "event":"https://sandiego.cityiq.io/api/v2/event/",
-        "websocket": "wss://https://sandiego.cityiq.io/api/v2/websocket", 
-        "developer": "PublicAccess:qPKIadEsoHjyh226Snz7",
-        "parking": "SD-IE-PARKING",
-        "environment": "SD-IE-ENVIRONMENTAL",
-        "pedestrian": "SD-IE-PEDESTRIAN",
-        "traffic": "SD-IE-TRAFFIC",
-        "bicycle": "SD-IE-BIKE", 
-        "bbox": "33.396515:-117.778637,32.467246:-115.638979"
+        "metadata":"https://portland.cityiq.io/api/v2/metadata/",
+        "event":"https://portland.cityiq.io/api/v2/event/",
+        "websocket": "wss://portland.cityiq.io/api/v2/websocket", 
+        "developer": "a1bd908f-1930-abe2-01db-20c3d34053e8:y7pFhvPx6ixYDpJ",
+        "parking": "Portland-IE-PARKING",
+        "environment": "Portland-IE-ENVIRONMENTAL",
+        "pedestrian": "Portland-IE-PEDESTRIAN",
+        "traffic": "Portland-IE-TRAFFIC",
+        "bicycle": "Portland-IE-BICYCLE",
+        "bbox": "45.522295:-122.654883,45.49452:-122.537388"
     }
 }
